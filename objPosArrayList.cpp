0d8646188ba12e4335fe2fef8163d5df342f66f6
azamn2 <azamn2@mcmaster.ca>
2024-12-01 01:11:42 -0500

    Iteration 2A

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -3,3 +3,76 @@
 // Paste your Tested implementation here.
 // Paste your Tested implementation here.
 // Paste your Tested implementation here.
+
+//constructor
+objPosArrayList::objPosArrayList()
+{
+    listSize = 0;
+    arrayCapacity = ARRAY_MAX_CAP;
+    aList = new objPos[arrayCapacity];
+}
+
+//deconstructor
+objPosArrayList::~objPosArrayList()
+{
+    delete[] aList;
+}
+
+//getSize
+int objPosArrayList::getSize() const
+{
+    return listSize;
+}
+
+//assign thisPos to first element of aList
+void objPosArrayList::insertHead(objPos thisPos)
+{
+    //shift elements 
+    int i;
+    for (i = listSize; i > 0; i--)
+    {
+        aList[i] = aList[i - 1];
+    }
+
+    //assign to first
+    aList[0] = thisPos;
+    listSize++;
+}
+
+//add thisPos to end of te list 
+void objPosArrayList::insertTail(objPos thisPos)
+{
+    aList[listSize] = thisPos;
+    listSize++;
+}
+
+//
+void objPosArrayList::removeHead()
+{
+    for(int i = 0; i < listSize - 1; i++)
+    {
+        aList[i] = aList[i + 1];
+    }
+    listSize--;
+}
+
+void objPosArrayList::removeTail()
+{
+    listSize--;
+}
+
+objPos objPosArrayList::getHeadElement() const
+{
+    return aList[0];
+}
+
+objPos objPosArrayList::getTailElement() const
+{
+    return aList[listSize - 1];
+}
+
+objPos objPosArrayList::getElement(int index) const
+{
+    return aList[index];
+}
+
